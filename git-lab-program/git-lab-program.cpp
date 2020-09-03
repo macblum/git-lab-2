bfd6d27413b195a6ade63d93bc2881d2c1827dd3
macblum <mb666418@ohio.edu>
2020-09-03 18:07:05 -0400

    git-lab-program file commit

---
+++ git-lab-program/git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cpp
+ *      Author: Mac Blum
+ *        Date: September 03, 2020
+ * Description: Lab #2.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const* argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
